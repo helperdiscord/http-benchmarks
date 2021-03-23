3535f44993622bd8323aa46153b6520d228f9639
Nytelife26 <xtylerjrx@gmail.com>
2021-03-23 12:22:14 +0000

    refactor: cleanup index

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,20 @@ import * as http from "http";
 // @ts-ignore: No declarations
 import { CompletionLogger, CycleLogger } from "@kludge-cs/bench-utils";
 import Benchmark from "benchmark";
+import { Client } from "undici";
 import type { Deferred } from "benchmark";
 import { URL } from "url";
 import centra from "centra";
-import pi from 'petitio';
 import fetch from "node-fetch";
 import got from "got";
+import pi from "petitio";
 import request from "request";
-import pkg from 'undici';
-const {Client} = pkg;
 
 const url = new URL("http://127.0.0.1:8080");
 const urlString = url.toString();
 const defer = {"defer": true};
 
-const client = new Client(url, { pipelining: 10 })
+const client = new Client(url, { pipelining: 10 });
 new Benchmark.Suite()
 	.add("got - promise", async (deferred: Deferred) => {
 		await got(url, constants.gotOptions);
@@ -40,11 +39,10 @@ new Benchmark.Suite()
 		await req.send();
 		deferred.resolve();
 	}, defer)
-	.add("petitio - promise", (deferred: Deferred) => {
-		const req = pi(url).client(client, true).option('pipelining', 10);
-		req.send().then(() => {
-			deferred.resolve();
-		});
+	.add("petitio - promise", async (deferred: Deferred) => {
+		const req = pi(url).client(client, true);
+		await req.send();
+		deferred.resolve();
 	}, defer)
 	.add("http - stream", (deferred: Deferred) => {
 		http.request(urlString, constants.httpsOptions, (response) => {
